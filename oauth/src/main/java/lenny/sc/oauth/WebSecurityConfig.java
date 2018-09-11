b87d198f3dfb5130e286a580114582a9e71a4c7e
xuequnmou@aspirecn.com <Abcd1234>
2018-09-11 18:17:47 +0800

    Merge branch 'master' of https://github.com/lennyxue/spring-cloud
    # Conflicts:
    #	sc-disco/src/main/resources/application.yml
    #	sc-feign/src/main/java/lenny/sc/scfeign/ScFeignApplication.java
    #	sc-ribbon/src/main/java/lenny/sc/scribbon/ScRibbonController.java
    #	sc-zuul/src/main/java/lenny/sc/sczuul/TokenFilter.java
    #	sc-zuul/src/main/resources/application.yml

---
+++ oauth/src/main/java/lenny/sc/oauth/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package lenny.sc.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
