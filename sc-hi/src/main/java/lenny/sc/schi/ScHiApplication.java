54aac3ea1e8ee7f0bb1ac67fc7aed99cd8b85a97
xuequnmou@aspirecn.com <Abcd1234>
2018-09-20 18:31:00 +0800

    add redis

--- sc-hi/src/main/java/lenny/sc/schi/ScHiApplication.java
+++ sc-hi/src/main/java/lenny/sc/schi/ScHiApplication.java
@@ -1,9 +1,13 @@
 package lenny.sc.schi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +17,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ScHiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScHiApplication.class, args);
-	}
+     @Autowired
+     RedisTemplate redisTemplate;
 
-	@Value("${server.port}")
+    public static void main(String[] args) {
+        SpringApplication.run(ScHiApplication.class, args);
+
+//save object into redis
+        ScHiApplication scHiApplication = new ScHiApplication();
+    }
+
+
+    @Value("${server.port}")
     String port;
-	@RequestMapping("/hi")
-    public String hi(@RequestParam String name){
+
+    @RequestMapping("/hi")
+    public String hi(@RequestParam String name) {
         return "Hi, This is " + name + " of port " + port;
 
-    }}
+    }
+    @RequestMapping("/redis")
+    public String redis(@RequestParam String name) {
+        redisTemplate.opsForHash().put(name, name, System.currentTimeMillis());
+        return "Hi, This is redis value " + redisTemplate.opsForHash().get(name, name);
+
+    }
+
+}
